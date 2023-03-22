e4598143106c5084ede5a88ebfe6efe2f1eff0c5
Alejandra Giannattasio <alegiannattasioxd@gmail.com>
2023-03-22 14:02:40 -0400

    Archivos reto 18

--- js/index.js
+++ js/index.js
@@ -1,83 +1,63 @@
-function cargarCarousel() {
-	$('#recipeCarousel').carousel({
-        interval: 4000
-    })
-
-    $('.carousel .carousel-item').each(function() {
-        var minPerSlide = 4;
-        var next = $(this).next();
-        if (!next.length) {
-            return;
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        for (var i = 0; i < minPerSlide; i++) {
-            next = next.next();
-            if (!next.length) {
-                return;
-            }
-
-            next.children(':first-child').clone().appendTo($(this));
-        }
-    })
-}
-
-
-function cargarListadoEstudiantes(listado) {
-	if(listado.length <= 0) {return;}
-	let listaEstudiantes = document.querySelector("#lista-estudiantes");
-	let itemContenedor = document.getElementById("plantilla-estudiante").querySelector("li");
-	let foto = itemContenedor.querySelector(".foto-estudiante");
-	let nombre = itemContenedor.querySelector(".nombre-estudiante");
-	listaEstudiantes.innerHTML = "";
-	i = 0;
-	for(let estudiante of listado) {
-		foto.src = estudiante.imagen;
-		itemContenedor.id = estudiante.ci;
-		nombre.innerText = estudiante.nombre;
-		nuevoEstudiante = document.importNode(itemContenedor, true);
-		if(i === 0) {
-			console.log("entre");
-			nuevoEstudiante.classList.add("active");
-		}
-		listaEstudiantes.appendChild(nuevoEstudiante);
-		++i;
-	}
-	cargarCarousel();
-}
-
-
-function cargarTextoInterfaz(config) {
-	let sitio = document.querySelector("#sitio");
-	let saludo = document.querySelector("#saludo");
-	let inputTextoNombre = document.querySelector("#input-texto-nombre");
-	let botonBuscar = document.querySelector("#boton-buscar");
-	let copyritht = document.querySelector("#copyright");
-	let tituloPestania = document.querySelector("#titulo-pestania");
-	nombreSesionActual = "Alejandra Giannattasio";
-	tituloPestania.innerHTML = `${config.sitio[0]}${config.sitio[1]} ${config.sitio[2]}`;
-	sitio.innerHTML = `${config.sitio[0]}<small>${config.sitio[1]}</small> ${config.sitio[2]}`;
-	saludo.innerHTML = `${config.saludo}, ${nombreSesionActual}`;
-	inputTextoNombre.placeholder = `${config.nombre}`;
-	botonBuscar.value = `${config.buscar}`;
-	copyritht.innerHTML = `${config.copyRight}`;
-}
-
-let msjNoEncontrado = config["msj_no_encontrado"];
-function busquedaEstudiante(event) {
-	let nombre = document.querySelector("#input-texto-nombre").value;
-	if(nombre.length <= 0) {
-		cargarListadoEstudiantes(listado);
-	} else {
-		ciList = listado.filter( estudiante => estudiante.nombre.toLowerCase().includes( nombre.toLowerCase() ) );
-		if(ciList.length <= 0) {
-			document.getElementById("lista-estudiantes").innerHTML = "<p class='msj'>" + msjNoEncontrado.replace("[query]", nombre) + "<p>";
-		} else {
-			cargarListadoEstudiantes(ciList);
-		}
-	}
-}
-
-document.getElementById("boton-buscar").onclick = busquedaEstudiante;
-cargarTextoInterfaz(config);
-cargarListadoEstudiantes(listado);
+// profile info
+function displayPerfil(perfil, img_src) {
+	// document.getElementsByClassName("saludo")[0].innerHTML += perfil.nombre;
+	document.getElementById("foto").src = img_src;
+	document.getElementById("perfil-nombre").innerText = perfil.nombre;
+	document.getElementById("perfil-descripcion").innerText = perfil.descripcion;
+	document.getElementById("perfil-color").innerText = perfil.color;
+	document.getElementById("perfil-libro").innerText = perfil.libro;
+	document.getElementById("perfil-musica").innerText = perfil.musica;
+	document.getElementById("perfil-juego").innerText = perfil.video_juego;
+	document.getElementById("perfil-lenguajes").innerText = perfil.lenguajes.join(", ") ;
+	document.getElementById("email").innerHTML = config_email.replace("[email]", `<a href="mailto:${perfil.email}">${perfil.email}</a>`);
+	$("footer").css("position", "relative");
+	$("#contenido-perfil").css("display", "block");
+  }
+  
+  function clickPerfil(ci, img_src) {
+  
+	// console.log(ci);
+	// console.log(img_src);
+  
+	$.get('/getDatos.php', {ci: String(ci)}, data => {
+	  displayPerfil( JSON.parse(data), img_src );
+	});
+	
+  }
+  
+  // drag to scroll 
+  const slider = document.querySelector('.scrolling-wrapper');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  let btn_strength = 750;
+  
+  slider.addEventListener('mousedown', (e) => {
+	isDown = true;
+	slider.classList.add('active');
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener('mouseleave', () => {
+	isDown = false;
+	slider.classList.remove('active');
+  });
+  slider.addEventListener('mouseup', () => {
+	isDown = false;
+	slider.classList.remove('active');
+  });
+  slider.addEventListener('mousemove', (e) => {
+	if(!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = (x - startX); //scroll-fast
+	slider.scrollLeft = scrollLeft - walk;
+  });
+  
+  $("#btn-scroller-right")[0].addEventListener('click', e => {
+	slider.scrollLeft += btn_strength;
+  });
+  
+  $("#btn-scroller-left")[0].addEventListener('click', e => {
+	slider.scrollLeft -= btn_strength;
+  });
