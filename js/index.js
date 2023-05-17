d95e31a5072b6578ed91b99293f03f791608ab73
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-05-17 16:45:03 -0400

    archivos reto 12

--- js/index.js
+++ js/index.js
@@ -1,46 +1,83 @@
-document.title = `${config.sitio[0]} ${config.sitio[1]} ${config.sitio[2]}`;
+document.getElementsByTagName('title')[0].innerHTML = config.sitio[0] + config.sitio[1] + ' ' + config.sitio[2];
 
-const saludo = document.getElementsByClassName('saludo')[0];
-saludo.innerHTML = `${config.saludo}, ${saludo.innerHTML}`;
+document.getElementsByClassName('logo')[0].innerHTML =
+	config.sitio[0] + '<small>' + config.sitio[1] + '</small> ' + config.sitio[2];
 
-const footer = document.getElementsByTagName('footer')[0];
-footer.innerText = config.copyRight;
+document.getElementsByClassName('saludo')[0].innerHTML = config.saludo + ', Jesus Hernandez';
 
-const logo = document.getElementsByClassName('logo')[0];
-logo.innerHTML = `${config.sitio[0]} <small>${config.sitio[1]}</small> ${config.sitio[2]}`;
+const busqueda = document.getElementsByClassName('busqueda')[0];
+busqueda.getElementsByTagName('input')[0].placeholder = config.nombre;
 
-const buscar = document.getElementsByTagName('input');
-buscar[0].placeholder = 'Nombre...';
-buscar[1].value = config.buscar;
+function convertirListado(listado) {
+	const resultado = listado.reduce((acc, current, currentIndex) => {
+		if (currentIndex === 0)
+			return (
+				acc + `<li class="carousel-item col active"><img src="./${current.imagen}"><a>${current.nombre}</a></li>`
+			);
+		return acc + `<li class="carousel-item col"><img src="./${current.imagen}"><a>${current.nombre}</a></li>`;
+	}, '');
 
-const form = document.getElementsByTagName('form')[0];
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-});
+	return resultado;
+}
 
-const estudiantes = document.getElementsByTagName('ul')[1];
-
-listado.forEach((student) => {
-	const li = document.createElement('li');
-	li.innerHTML = `<img src="${student.imagen}"> <span>${student.nombre}</span>`;
-	estudiantes.appendChild(li);
-});
-
-buscar[0].addEventListener('change', (input) => {
-	estudiantes.innerHTML = '';
-
-	const list = listado.filter((item) => item.nombre.includes(input.target.value));
-
-	if (list.length === 0) {
-		const error = document.createElement('div');
-		error.innerHTML = `No hay alumnos que tengan en su nombre: ${input.target.value}`;
-		error.classList.add('msg_error');
-		estudiantes.appendChild(error);
-	} else {
-		list.forEach((item) => {
-			const li = document.createElement('li');
-			li.innerHTML = `<img src="${item.imagen}"> <span>${item.nombre}</span>`;
-			estudiantes.appendChild(li);
-		});
+const inputElement = busqueda.getElementsByTagName('input')[1];
+const inputText = busqueda.getElementsByTagName('input')[0];
+inputText.addEventListener('keypress', (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
 	}
 });
+inputElement.value = config.buscar;
+
+inputElement.addEventListener('click', () => {
+	const value = inputText.value;
+	let length = value.length;
+	if (length > 0) {
+		let resp = listado.filter((person) => person.nombre.slice(0, length) === value);
+
+		if (resp.length === 0) {
+			console.log('no existe');
+			document.getElementsByTagName('ul')[1].innerHTML = `No hay alumnos que tengan en su nombre: ${value}`;
+		} else {
+			document.getElementsByTagName('ul')[1].innerHTML = convertirListado(resp);
+		}
+	} else {
+		document.getElementsByTagName('ul')[1].innerHTML = convertirListado(listado);
+	}
+});
+
+document.getElementsByTagName('footer')[0].innerHTML = config.copyRight;
+
+// $('.carousel-inner').append(list_item);
+$('.carousel-inner').append(convertirListado(listado));
+
+$('.carousel-inner .carousel-item:first').addClass('active');
+
+$('.carousel').carousel({ interval: 3000 });
+
+$('#persons').on('slid.bs.carousel', function (e) {
+	const total = 5;
+	const items = $('.carousel-item');
+	const index = $(e.relatedTarget).index();
+
+	const size = total - (items.length - index);
+
+	if (index >= items.length - (total - 1)) {
+		for (let i = 0; i < size; i++) {
+			let carouselInner = $('.carousel-inner');
+			let carouselItems = $('.carousel-item');
+
+			let itemToMove = carouselItems.eq(i);
+			if (e.direction === 'right') {
+				itemToMove = carouselItems.eq(0);
+			}
+			itemToMove.appendTo(carouselInner);
+		}
+	}
+});
+
+// const list_item = listado.reduce((acc, current, currentIndex) => {
+// 	if (currentIndex == 0)
+// 		return (acc += `<li class="carousel-item col active"> <img src="./${current.imagen}"> <a> ${current.nombre} </a> </li>`);
+// 	return (acc += `<li class="carousel-item col"> <img src="./${current.imagen}"> <a> ${current.nombre} </a> </li>`);
+// }, ' ');
