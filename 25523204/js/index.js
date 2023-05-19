ae206317f6807de15dc595c8f7e30c2392fbbddc
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-05-19 13:55:15 -0400

    archivos reto 14

--- 25523204/js/index.js
+++ 25523204/js/index.js
@@ -1,3 +1,32 @@
+let hovering = true;
+
+const foto = $('#foto');
+const svgShow = $('#svg-show');
+const textSvg = $('#text-svg');
+
+foto.hover(() => {
+	if (hovering) {
+		svgShow.css({
+			transform: 'translateY(0%)',
+			opacity: '75%',
+		});
+		textSvg.css({
+			transform: 'translateY(0%)',
+			opacity: '1',
+		});
+	} else {
+		svgShow.css({
+			opacity: '0',
+			transform: 'translateY(-70%)',
+		});
+		textSvg.css({
+			transform: 'translateY(-80%)',
+			opacity: '0',
+		});
+	}
+	hovering = !hovering;
+});
+
 document.title = `${config.sitio[0]} ${config.sitio[1]} ${config.sitio[2]}`;
 document.getElementsByClassName(
 	'logo'
@@ -10,26 +39,25 @@ const enlace = (busquedaLi.querySelector('a').innerHTML = config.home);
 
 document.getElementsByClassName('saludo')[0].innerHTML = config.saludo + ', ' + perfil.nombre;
 
-const imagen = document.getElementsByClassName('profile-image')[0];
+const imagen = document.getElementById('profile-image');
 imagen.src = perfil.imagen;
 imagen.alt = perfil.nombre;
 
-const nombreElemento = document.querySelector('.profile-box div h1');
+const nombreElemento = document.getElementById('nombre');
 nombreElemento.textContent = perfil.nombre;
 
 const div = document.querySelector('.profile-box div');
 
-const elementosP = div.querySelectorAll('p');
-
-elementosP[0].innerHTML = perfil.descripcion;
+document.getElementById('descripcion').textContent = perfil.descripcion;
 
 document.getElementById('color-favorito').textContent = `${config.color}: ${perfil.color}`;
 document.getElementById('libro-favorito').textContent = `${config.libro}: ${perfil.libro}`;
 document.getElementById('estilo-musica').textContent = `${config.musica}: ${perfil.musica}`;
 document.getElementById('videojuegos').textContent = `${config.video_juego}: ${perfil.video_juego}`;
-elementosP[4].innerHTML = `${config.lenguajes}: <strong>${perfil.lenguajes}</strong>`;
 
-document.getElementsByClassName('text_link')[0].innerHTML = config.email.replace(
+document.getElementById('lenguajes').innerHTML = `${config.lenguajes}: <strong>${perfil.lenguajes}</strong>`;
+
+document.getElementById('link').innerHTML = config.email.replace(
 	'[email]',
 	`<a href="mailto:${perfil.email}">${perfil.email}</a>`
 );
